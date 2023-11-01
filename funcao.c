a7d6928d0cbabc4f01d3b80c1cfa28ec000ec267
isbelah <isabela.soares43@etec.sp.gov.br>
2023-11-01 17:15:28 -0300

    bb

--- funcao.c
+++ funcao.c
@@ -17,4 +17,27 @@ int main(int argc, char *argv[]) {
 		printf("\nDigite o primeiro número:");
 		scanf("%d", &n1);
 		printf("\nDigite o segundo número:");
-		scanf("%d", &n2);		
+		scanf("%d", &n2);	
+	switch(opera){
+		case 1:
+			total=n1+n2;
+			break;
+		case 2:
+			total=n1-n2;
+			break;
+		case 3:
+			total=n1*n2;
+			break;
+		case 4:
+			total=n1/n2;
+			break;
+		}
+	if(opera>4){
+		printf("\nDigite uma operação que exista");
+	}
+	else if(opera<1){
+		printf("\nDigite uma operação que exista");
+	}
+	printf("\nTotal da operação:%d", &total);
+	printf("\nDeseja fazer outra operação:\nSim[0]\nNão\n: ");
+	scanf("%d", &conta);
